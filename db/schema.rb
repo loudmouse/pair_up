1df91a06777de80e4a4f3f86943c8b6e5dc2363a
Nolan Neeley <nolanneeley@gmail.com>
2018-04-15 10:10:41 -0500

    added date field to event MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414194907) do
+ActiveRecord::Schema.define(version: 20180415150234) do
 
   create_table "events", force: :cascade do |t|
     t.integer "user_id"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
